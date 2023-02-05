533b11658c0125f6f12a07aac911bf2f9ef5b55f
heline-m <eline.malherbe@epsi.fr>
2023-02-05 15:35:35 +0100

    Etape4 relier la langue et la période à celle du système

---
+++ OHCE.Console/Adapters/SystemLangueAdapter.cs
@@ -0,0 +1,20 @@
+﻿using OHCE.Langues;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OHCE.Console.Adapters
+{
+    internal class SystemLangueAdapter
+    {
+        public static ILangue LangueActuelle
+            => CultureInfo.InstalledUICulture.TwoLetterISOLanguageName switch
+            {
+                "fr" => new LangueFrançaise(),
+                "en" => new LangueAnglaise(),
+            };
+    }
+}
